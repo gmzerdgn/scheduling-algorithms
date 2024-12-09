ac7639f84a2ceb69d9be356a23ab77807540907e
= <=>
2024-12-09 14:00:51 +0300

    processes

--- README.md
+++ README.md
@@ -2,7 +2,3 @@ RMA DMA EDF LLF
 
 scheduling algorithms implementation (c++)
 
-TODO 
-[ ] cpp syntax/commenting
-[ ] processes as objects (period, time) (and other input forms)
-[ ] output in a nice way
--- main.cpp
+++ main.cpp
@@ -2,7 +2,37 @@
 
 using namespace std;
 
+/*
 TODO 
-[ ] cpp syntax/commenting
+[x] cpp syntax/commenting
+[ ] read from external file 
 [ ] processes as objects (period, time) (and other input forms)
-[ ] output in a nice way
+[ ] output in a nice way 
+*/
+
+class Process {
+    private:
+        float delay; 
+        float period; 
+        float executionTime;
+        float deadline;
+        string name;
+    public:
+        Process (string name, float period, float executionTime); //constructor with period and execution time only
+        Process (string name, float period, float executionTime, float deadline);
+};
+
+Process::Process (string name, float period, float executionTime){
+    name = name;
+    period = period;
+    executionTime = executionTime;
+};
+
+Process::Process (string name, float period, float executionTime, float deadline){
+    name = name;
+    period = period;
+    executionTime = executionTime;
+    deadline = deadline;
+}
+
+
